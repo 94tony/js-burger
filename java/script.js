cd5414250fa13379a46c49b013b0ec04babd0ae3
94tony <dagatatony94@gmail.com>
2021-06-08 12:48:20 +0200

    struttura 1.5

--- java/script.js
+++ java/script.js
@@ -1,3 +1,4 @@
+// VOTTONE / RESULT
 var generator = document.getElementById("random-number")
 var ResultsElement = document.getElementById("Results")
 // prezzo
@@ -5,22 +6,22 @@ var discountCopoun = 30
 var userCoupon  = document.getElementById("discount")
 
 generator.addEventListener('click' , function() {
-var sum = 20;
+    var sum = 20;
 
-var selection = document.getElementsByClassName("toppings"); 
+    var selection = document.getElementsByClassName("toppings"); 
 
-for(var x = 0 ;  x < selection.length  ; x++){
-   if(selection[x].checked){
-       sum += parseInt(selection[x].value)
-   }
-   if(userCoupon.value.length === 6){
-       var prezzosconto = sum-sum/100 *50
-    document.getElementById('Results').innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  + parseInt(prezzosconto).toFixed(2);
-}
- 
- else{
-    ResultsElement.innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  +  sum.toFixed(2);
- }
+    for(var x = 0 ;  x < selection.length  ; x++){
+        if(selection[x].checked){
+            sum += parseInt(selection[x].value)
+        }
+    }
 
-}
+    if(userCoupon.value.length === 6){
+        var prezzosconto = sum-sum/100 *50
+        document.getElementById('Results').innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  + parseInt(prezzosconto).toFixed(2);
+    } else {
+        ResultsElement.innerHTML = 'Il costo del tuo burger è:  ' +  " $ "  +  sum.toFixed(2);
+    }
+
+
 })
